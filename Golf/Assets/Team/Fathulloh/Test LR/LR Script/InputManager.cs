6c2b25cff46f3d5a6fee191771feea9baa125f10
RustamMuhammadFathulloh <blackgoold2001@gmail.com>
2023-08-27 18:43:39 +0500

    LineRenderer Golf ball arrow main parts finished.

--- Golf/Assets/Team/Fathulloh/Test LR/LR Script/InputManager.cs
+++ Golf/Assets/Team/Fathulloh/Test LR/LR Script/InputManager.cs
@@ -6,33 +6,71 @@ namespace Golf_LineRenderer2
 {
     public class InputManager : MonoBehaviour
     {
-        //public static InputManager Instance;
         public GameObject MainBall;
-        [SerializeField] private LineRenderer _trajectoryLine;
         public GameObject CircleObj;  //F+
-
-
-        void Update()
-        {
-            //ShowTrajectoryLine();
-        }
-
+        [SerializeField] private LineRenderer _trajectoryLine;
+        public LineRenderer WhiteArrowWay;
+        
 
         public void ShowTrajectoryLine()
         {
             if (Input.GetMouseButton(0))
             {                
-                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition - MainBall.GetComponent<DragDrop>().MousePos /*CircleObj.GetComponent<CircleMove>().mousePos*/);
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition - MainBall.GetComponent<DragDrop>().MousePos);
                 Vector3 mainBallPos = MainBall.transform.position;
                 _trajectoryLine.enabled = true;
                 _trajectoryLine.positionCount = 2;
 
-                //CircleObj.transform.position = new Vector3(mousePos.x, mainBallPos.y, mousePos.z); //F+
+                float lineLength = Vector3.Distance(CircleObj.transform.position, mainBallPos);   //F+
+                if (lineLength < redLineLength)
+                {
+                    if (lineLength < greenLineLength)                    {
+                        //Debug.Log("green");
+                        DrawWhiteArrowWay(CircleObj.transform.position, mainBallPos, greenColor);
+                    }
+                    else if (lineLength < yellowLineLength)                    {
+                        //Debug.Log("yellow");
+                        DrawWhiteArrowWay(CircleObj.transform.position, mainBallPos, yellowColor);
+                    }
+                    else if (lineLength < redLineLength)                    {
+                        //Debug.Log("red");
+                        DrawWhiteArrowWay(CircleObj.transform.position, mainBallPos, redColor);
+                    }
 
-                _trajectoryLine.SetPosition(0, CircleObj.transform.position);
-                _trajectoryLine.SetPosition(1, mainBallPos);;
+                    _trajectoryLine.SetPosition(0, CircleObj.transform.position);
+                    _trajectoryLine.SetPosition(1, mainBallPos); ;
+                }
+                
             }
         }
-        
+
+
+        Color greenColor = new(0.27f, 0.85f, 0.2f);
+        Color yellowColor = new(1, 1, 0);
+        Color redColor = new(0.83f, 0.16f, 0.05f);
+
+        readonly float greenLineLength = 4.0f;
+        readonly float yellowLineLength = 7.0f;
+        [HideInInspector] public readonly float redLineLength = 10.0f;
+
+
+        void DrawWhiteArrowWay(Vector3 vec1, Vector3 vec2, Color newColor)
+        {
+            WhiteArrowWay.SetColors(newColor, newColor);
+
+            //Material newMat = new(WhiteArrowWay.material);   // materialni rangini o‘zgartirish ham juda yaxshi ideya edi. Biroq rangi o‘zgarmadi. 
+            //newMat.color = newColor;
+
+            float lineLength = Vector3.Distance(vec1, vec2);
+
+            //Debug.Log("lineLength = " + lineLength);
+            CircleObj.GetComponent<SpriteRenderer>().color = newColor;
+
+            WhiteArrowWay.positionCount = 2;
+            WhiteArrowWay.SetPosition(0, vec1);
+            WhiteArrowWay.SetPosition(1, vec2);           
+        }
+
+
     }
 }
