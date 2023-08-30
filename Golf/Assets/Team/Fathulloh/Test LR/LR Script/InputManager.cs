c83e601645da926e32685b866835d5604f133e25
RustamMuhammadFathulloh <blackgoold2001@gmail.com>
2023-08-30 14:44:49 +0500

    Line Renderer

--- Golf/Assets/Team/Fathulloh/Test LR/LR Script/InputManager.cs
+++ Golf/Assets/Team/Fathulloh/Test LR/LR Script/InputManager.cs
@@ -10,7 +10,16 @@ namespace Golf_LineRenderer2
         public GameObject CircleObj;  //F+
         [SerializeField] private LineRenderer _trajectoryLine;
         public LineRenderer WhiteArrowWay;
-        
+
+
+        Color greenColor = new(0.27f, 0.85f, 0.2f);
+        Color yellowColor = new(1, 1, 0);
+        Color redColor = new(0.83f, 0.16f, 0.05f);
+
+        readonly float greenLineLength = 4.0f;
+        readonly float yellowLineLength = 7.0f;
+        [HideInInspector] public readonly float redLineLength = 10.0f;
+
 
         public void ShowTrajectoryLine()
         {
@@ -25,33 +34,23 @@ namespace Golf_LineRenderer2
                 if (lineLength < redLineLength)
                 {
                     if (lineLength < greenLineLength)                    {
-                        //Debug.Log("green");
                         DrawWhiteArrowWay(CircleObj.transform.position, mainBallPos, greenColor);
                     }
                     else if (lineLength < yellowLineLength)                    {
-                        //Debug.Log("yellow");
                         DrawWhiteArrowWay(CircleObj.transform.position, mainBallPos, yellowColor);
                     }
                     else if (lineLength < redLineLength)                    {
-                        //Debug.Log("red");
                         DrawWhiteArrowWay(CircleObj.transform.position, mainBallPos, redColor);
                     }
 
-                    _trajectoryLine.SetPosition(0, CircleObj.transform.position);
-                    _trajectoryLine.SetPosition(1, mainBallPos); ;
+                    _trajectoryLine.SetPosition(0, NewPosForCircle(CircleObj.transform.position, mainBallPos));
+                    //_trajectoryLine.SetPosition(0, CircleObj.transform.position);
+                    _trajectoryLine.SetPosition(1, mainBallPos);
                 }
                 
             }
         }
-
-
-        Color greenColor = new(0.27f, 0.85f, 0.2f);
-        Color yellowColor = new(1, 1, 0);
-        Color redColor = new(0.83f, 0.16f, 0.05f);
-
-        readonly float greenLineLength = 4.0f;
-        readonly float yellowLineLength = 7.0f;
-        [HideInInspector] public readonly float redLineLength = 10.0f;
+        
 
 
         void DrawWhiteArrowWay(Vector3 vec1, Vector3 vec2, Color newColor)
@@ -60,17 +59,105 @@ namespace Golf_LineRenderer2
 
             //Material newMat = new(WhiteArrowWay.material);   // materialni rangini o‘zgartirish ham juda yaxshi ideya edi. Biroq rangi o‘zgarmadi. 
             //newMat.color = newColor;
+            //WhiteArrowWay.material = newMat;
 
-            float lineLength = Vector3.Distance(vec1, vec2);
-
-            //Debug.Log("lineLength = " + lineLength);
             CircleObj.GetComponent<SpriteRenderer>().color = newColor;
 
             WhiteArrowWay.positionCount = 2;
-            WhiteArrowWay.SetPosition(0, vec1);
-            WhiteArrowWay.SetPosition(1, vec2);           
+            WhiteArrowWay.SetPosition(0, NewPosForCircle(vec1, vec2));
+            //WhiteArrowWay.SetPosition(0, vec1);
+            WhiteArrowWay.SetPosition(1, vec2);            
         }
 
 
+        /// <summary>
+        /// Circle uchun yangi pozitsiyani belgilab beruvchi metod.
+        /// </summary>
+        /// <param name="vecCircle">Aylananing pozitsiyasi</param>
+        /// <param name="vecBall">Golf koptogining pozitsiyasi</param>
+        /// <returns></returns>
+        Vector3 NewPosForCircle(Vector3 vecCircle, Vector3 vecBall)
+        {
+            return SetNewDistance();   // F++
+
+            //float moveX = 0.64f;
+            //float moveZ = 0.64f;
+
+            //if ((vecCircle.x > vecBall.x) && (vecCircle.z == vecBall.z))
+            //{
+            //    Debug.Log("1");
+            //    return new Vector3(vecCircle.x - moveX, vecCircle.y, vecCircle.z);
+            //}
+            //else if ((vecCircle.x < vecBall.x) && (vecCircle.z == vecBall.z))
+            //{
+            //    Debug.Log("2");
+            //    return new Vector3(vecCircle.x + moveX, vecCircle.y, vecCircle.z);
+            //}
+            //else if ((vecCircle.z > vecBall.z) && (vecCircle.x == vecBall.x))
+            //{
+            //    Debug.Log("3");
+            //    return new Vector3(vecCircle.x, vecCircle.y, vecCircle.z - moveZ);
+            //}
+            //else if ((vecCircle.z < vecBall.z) && (vecCircle.x == vecBall.x))
+            //{
+            //    Debug.Log("4");
+            //    return new Vector3(vecCircle.x, vecCircle.y, vecCircle.z + moveZ);
+            //}
+            //else if ((vecCircle.x > vecBall.x) && (vecCircle.z > vecBall.z))
+            //{
+            //    Debug.Log("5");
+            //    return new Vector3(vecCircle.x - moveX / 3, vecCircle.y, vecCircle.z - moveZ / 3);
+            //}
+            //else if ((vecCircle.x < vecBall.x) && (vecCircle.z > vecBall.z))
+            //{
+            //    Debug.Log("6");
+            //    return new Vector3(vecCircle.x + moveX / 3, vecCircle.y, vecCircle.z - moveZ / 3);
+            //}
+            //else if ((vecCircle.x < vecBall.x) && (vecCircle.z < vecBall.z))
+            //{
+            //    Debug.Log("7");
+            //    return new Vector3(vecCircle.x + moveX / 3, vecCircle.y, vecCircle.z + moveZ / 3);
+            //}
+            //else if ((vecCircle.x > vecBall.x) && (vecCircle.z < vecBall.z))
+            //{
+            //    Debug.Log("8");
+            //    return new Vector3(vecCircle.x - moveX / 3, vecCircle.y, vecCircle.z + moveZ / 3);
+            //}
+            //else
+            //{
+            //    return new Vector3(vecCircle.x, vecCircle.y, vecCircle.z);
+            //}
+        }
+
+
+        public List<float> distances;
+
+        Vector3 SetNewDistance()
+        {
+            for (int i = 0; i < CircleObj.transform.childCount; i++)
+            {
+                GameObject newObj = CircleObj.transform.GetChild(i).gameObject;
+                float dis = Vector3.Distance(MainBall.transform.position, newObj.transform.position);
+                distances.Add(dis);
+            }
+
+            float minValue = distances[0];
+            int minIndex = 0;
+
+            for (int i = 0; i < distances.Count; i++)
+            {
+                if (distances[i] < minValue)
+                {
+                    minValue = distances[i];
+                    minIndex = i;
+                }
+            }
+            Debug.Log(" minIndex = " + minIndex);
+
+            //Debug.Log( CircleObj.transform.GetChild(minIndex).name);
+            distances.Clear(); // F+
+            return CircleObj.transform.GetChild(minIndex).transform.position;
+        }
+        
     }
 }
