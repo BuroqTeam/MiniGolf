d5eef3df89cb38892a1a3cf6d5ef31087cf78e72
RustamMuhammadFathulloh <blackgoold2001@gmail.com>
2023-09-15 18:49:08 +0500

    MyBall return initialPos.
    Ball prefab changed.
    Physic material cahnged.
    SceneLoader script created.
    Ball and TriggerController script changed.

--- Golf/Assets/Scripts/Ball/TriggerController.cs
+++ Golf/Assets/Scripts/Ball/TriggerController.cs
@@ -12,41 +12,41 @@ namespace MiniGolf
 
         private void OnTriggerEnter(Collider other)
         {
-            //switch (other.tag)
-            //{
-            //    case "Coin":
-            //        Debug.Log("Coin");
-            //        gameEventsSO[0].Raise();
-            //        break;
-            //    case "Wall":
-            //        Debug.Log("Wall Trigger");
-            //        gameEventsSO[3].Raise();
-            //        break;
-            //    case "Water":
-            //        Debug.Log("Water");
-            //        gameEventsSO[5].Raise();
-            //        break;
-            //    case "Object":
-            //        Debug.Log("Object");
-            //        gameEventsSO[6].Raise();
-            //        break;
-            //    case "Sand":
-            //        Debug.Log("Sand");
-            //        gameEventsSO[4].Raise();
-            //        break;
-            //    case "Finish":
-            //        Debug.Log("Finish");
-            //        gameEventsSO[1].Raise();
-            //        break;
-            //    case "Out":
-            //        Debug.Log("Out");
-            //        gameEventsSO[7].Raise();
-            //        break;
-            //    case "Land":
-            //        Debug.Log("Land");
-            //        gameEventsSO[8].Raise();
-            //        break;
-            //}
+            switch (other.tag)
+            {
+                case "Coin":
+                    Debug.Log("Coin Trigger");
+                    gameEventsSO[0].Raise();
+                    break;
+                //case "Wall":
+                //    Debug.Log("Wall Trigger");
+                //    gameEventsSO[3].Raise();
+                //    break;
+                //case "Water":
+                //    Debug.Log("Water Trigger");
+                //    gameEventsSO[5].Raise();
+                //    break;
+                //case "Object":
+                //    Debug.Log("Object Trigger");
+                //    gameEventsSO[6].Raise();
+                //    break;
+                //case "Sand":
+                //    Debug.Log("Sand Trigger");
+                //    gameEventsSO[4].Raise();
+                //    break;
+                case "Finish":
+                    Debug.Log("Finish Trigger");
+                    gameEventsSO[1].Raise();
+                    break;
+                case "Out":
+                    Debug.Log("Out Trigger");
+                    gameEventsSO[7].Raise();
+                    break;
+                //case "Land":
+                //    Debug.Log("Land Trigger");
+                //    gameEventsSO[8].Raise();
+                //    break;
+            }
         }
 
 
@@ -55,42 +55,42 @@ namespace MiniGolf
         {
             if (collision.gameObject.CompareTag("Coin"))
             {
-                Debug.Log("Coin");
+                //Debug.Log("Coin");
                 gameEventsSO[0].Raise();
             }
             else if (collision.gameObject.CompareTag("Wall"))
             {
-                Debug.Log("Wall Trigger");
+                //Debug.Log("Wall");
                 gameEventsSO[3].Raise();
             }
             else if (collision.gameObject.CompareTag("Water"))
             {
-                Debug.Log("Water");
+                //Debug.Log("Water");
                 gameEventsSO[5].Raise();
             }
             else if (collision.gameObject.CompareTag("Object"))
             {
-                Debug.Log("Object");
+                //Debug.Log("Object");
                 gameEventsSO[6].Raise();
             }
             else if (collision.gameObject.CompareTag("Sand"))
             {
-                Debug.Log("Sand");
+                //Debug.Log("Sand");
                 gameEventsSO[4].Raise();
             }
-            else if (collision.gameObject.CompareTag("Finish"))
-            {
-                Debug.Log("Finish");
-                gameEventsSO[1].Raise();
-            }
-            else if (collision.gameObject.CompareTag("Out"))
-            {
-                Debug.Log("Out");
-                gameEventsSO[7].Raise();
-            }
+            //else if (collision.gameObject.CompareTag("Finish"))
+            //{
+            //    //Debug.Log("Finish");
+            //    gameEventsSO[1].Raise();
+            //}
+            //else if (collision.gameObject.CompareTag("Out"))
+            //{
+            //    //Debug.Log("Out");
+            //    gameEventsSO[7].Raise();
+            //}
             else if (collision.gameObject.CompareTag("Land"))
             {
-                Debug.Log("Land");
+                //Debug.Log("Land");
                 gameEventsSO[8].Raise();
             }
 
