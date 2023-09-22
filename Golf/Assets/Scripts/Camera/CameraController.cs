6cdaff5d054c20ea54e90dfc0396cc18203c32b0
RustamMuhammadFathulloh <blackgoold2001@gmail.com>
2023-09-22 18:51:50 +0500

    Out bug fixed. Added new texture
    3 new texture and 3 material.
    Some scripts changed.

--- Golf/Assets/Scripts/Camera/CameraController.cs
+++ Golf/Assets/Scripts/Camera/CameraController.cs
@@ -15,6 +15,8 @@ namespace MiniGolf
         private Vector3 _previousPosition;
         private Camera _mainCamera;
 
+        Vector3 lastPos;
+
         private void Awake()
         {
             _mainCamera = GetComponent<Camera>();
@@ -62,21 +64,44 @@ namespace MiniGolf
                     SetCamerePosition();
                     _previousPosition = _mainCamera.ScreenToViewportPoint(Input.mousePosition);
 
+                    lastPos = gameObject.transform.position;
                 }
             }
 
-
-
         }
+
+
+        bool _IsSimpleCameraMove = true;
 
         void SetCamerePosition()
         {
-            transform.position = Ball.position;
-            transform.Translate(new Vector3(0, 0.15f, -0.85f));
+            if (Ball.GetComponent<Ball>()._IsBallOut && _IsSimpleCameraMove)
+            {
+                //Ball.GetComponent<Ball>()._IsBallOut = false;
+                _IsSimpleCameraMove = false;
+                StartCoroutine(CameraMove());
+            }
+            else if (_IsSimpleCameraMove)
+            {
+                transform.position = Ball.position;
+                transform.Translate(new Vector3(0, 0.15f, -0.85f));
+            }
+            
+            //ransform.position = Ball.position;
+            //transform.Translate(new Vector3(0, 0.15f, -0.85f));
         }
 
 
-
+        IEnumerator CameraMove() // Ball tashqariga chiqib ketganda camerani avvalgi pozitsiyasiga qaytaradigan metod.
+        {
+            transform.DOMove(lastPos, 1);
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            
+            yield return new WaitForSeconds(1.2f);          // Buyerdagi vaqt Ball.cs ChangeCameraFields() metodini ichidagi uchinchi wait for second vaqti bilan teng bo'lsin.
+            Ball.GetComponent<Ball>()._IsBallOut = false;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            _IsSimpleCameraMove = true;
+        }
 
 
     }
