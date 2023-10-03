71d85fe73fa39328420dcd73935d19b62c4f7270
RustamMuhammadFathulloh <98743553+RustamMuhammadFathulloh@users.noreply.github.com>
2023-10-03 13:44:48 +0500

    How to Play need to add sprites
    How to play ning animatsiyasi va boardi yasaldi.
    How to playdagi buttonlar uchun va board uchun yangi spritelar qo'shish kerak.

---
+++ Golf/Assets/Scripts/HowToPlay/HowToPlay.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+namespace MiniGolf
+{
+    /// <summary>
+    /// Qanday o'ynashni tushuntiradigan animatsiya uchun script. //F
+    /// </summary>
+    public class HowToPlay : MonoBehaviour
+    {
+        [SerializeField] private RectMask2D rectMask2D;
+        [SerializeField] private Image lineImage;
+        [SerializeField] private GameObject handCursor;
+        [SerializeField] private GameObject ball;
+        [SerializeField] private GameObject circleGap;
+        [SerializeField] private GameObject circleForHand;
+
+        Vector3 ballPos;
+        Vector3 _rotation = new Vector3 (0, 0, 60);
+        [HideInInspector] public bool _isRotating = false;
+        private bool _isObjectActive = false;
+        Vector4 maskPadding;
+        
+        Color greenColor = new Color(0.25f, 1, 0.27f);
+        Color yellowColor = new Color(1, 0.97f, 0.17f);
+        Color redColor = new Color(1, 0.17f, 0.05f);
+
+        Image circleImage;
+
+
+        void Start()
+        {
+            ballPos = ball.transform.position;
+            circleImage = circleForHand.GetComponent<Image>();
+            _isRotating = true;
+            maskPadding = rectMask2D.padding;            
+        }
+
+        private void OnEnable()
+        {
+            _isObjectActive = true;
+            StartCoroutine(BallAnimation());
+            Debug.Log("OnEnable");
+        }
+
+        private void OnDisable()
+        {
+            _isObjectActive = false;
+            Debug.Log("OnDisable");
+        }
+
+        void Update()
+        {
+            if (_isRotating && _isObjectActive)
+            {
+                circleGap.transform.Rotate(_rotation * Time.deltaTime);
+            }                      
+        }
+
+
+        IEnumerator BallAnimation()
+        {
+            yield return new WaitForSeconds(1.5f);
+            _isRotating = false;
+            circleGap.SetActive(false);
+
+            handCursor.transform.position = new Vector3(ballPos.x + 65, ballPos.y - 65, ballPos.z);
+            circleForHand.transform.position = new Vector3(ballPos.x, ballPos.y, ballPos.z);
+
+            handCursor.SetActive(true);
+            circleForHand.SetActive(true);
+            yield return new WaitForSeconds(0.1f);
+
+            handCursor.GetComponent<RectTransform>().DOAnchorPosY(-255, 1.12f);
+            circleForHand.GetComponent<RectTransform>().DOAnchorPosY(-215, 1.12f);
+            yield return new WaitForSeconds(.05f);
+
+            while (rectMask2D.padding.y > 0)
+            {
+                rectMask2D.padding = new Vector4(0, rectMask2D.padding.y - 10, 0, 0);
+                yield return new WaitForSeconds(0.03f);
+
+                if (rectMask2D.padding.y < 60)
+                {
+                    lineImage.color = redColor;
+                    circleImage.color = redColor;
+                }
+                else if (rectMask2D.padding.y < 120)
+                {
+                    lineImage.color = yellowColor;
+                    circleImage.color = yellowColor;
+                }
+                else if (rectMask2D.padding.y > 120)
+                {
+                    lineImage.color = greenColor;
+                    circleImage.color = greenColor;
+                }
+
+                if (rectMask2D.padding.y <= 1)
+                {
+                    //Debug.Log("While ishladi");
+                    break;
+                }
+            }
+
+            yield return new WaitForSeconds(2.0f);
+            
+            _isRotating = true;
+            circleGap.SetActive(true);
+            rectMask2D.padding = maskPadding;
+            circleImage.color = greenColor;
+            lineImage.color = greenColor;
+
+            handCursor.SetActive(false);
+            circleForHand.SetActive(false);
+
+            StartCoroutine(BallAnimation());
+        }
+
+
+        
+    }
+}
