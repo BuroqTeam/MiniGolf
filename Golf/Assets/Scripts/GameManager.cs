7e4fbe8be91360422034227c44babe04ab2b4452
RustamMuhammadFathulloh <blackgoold2001@gmail.com>
2023-12-05 17:37:34 +0500

    Web GL bug

--- Golf/Assets/Scripts/GameManager.cs
+++ Golf/Assets/Scripts/GameManager.cs
@@ -16,10 +16,14 @@ namespace MiniGolf
 
         private ResultInfo _data;
 
-        //public TMP_Text ServerText1;
-        //public TMP_Text ServerText2;
-        //public TMP_Text ServerText3;
-        //public TMP_Text ServerText4;
+        public TMP_Text ServerResponceText;
+        public TMP_Text ServerText1;
+        public TMP_Text ServerText2;
+        public TMP_Text ServerText3;
+        public TMP_Text ServerText4;
+        public TMP_Text ServerText5;
+        public TMP_Text ServerText6;
+        public TMP_Text ServerText7;
 
         private void Awake()
         {
@@ -57,13 +61,22 @@ namespace MiniGolf
 
         public void UpdateResultInfo(string level, string numberOfHits, string coin)
         {
-            //_data.eventSq = "1";
-            //_data.roundNo = "1";
-            //_data.memberId = "1";
-            //_data.gameId = "1";
+            _data.eventSq = "2";
+            _data.roundNo = "2";
+            _data.memberId = "2";
+            _data.gameId = "2";
             _data.holeNo = level;
             _data.hole = numberOfHits;
             _data.star = coin;
+            
+            //PlayerPrefs.SetString("eventSq", _data.eventSq);
+            //PlayerPrefs.SetString("roundNo", _data.roundNo);
+            //PlayerPrefs.SetString("memberId", _data.memberId);
+            //PlayerPrefs.SetString("gameId", _data.gameId);
+            //PlayerPrefs.SetString("holeNo", _data.holeNo);
+            //PlayerPrefs.SetString("hole", _data.hole);
+            //PlayerPrefs.SetString("star", _data.star);
+
             WriteUserScoreToServer();
         }
 
@@ -104,9 +117,35 @@ namespace MiniGolf
 
         public async void WriteUserScoreToServer()
         {
-            ResponseResultInfo responce = await HttpService.SaveResultInfo(_data);            
-            Debug.Log(responce.result);
+            ResponseResultInfo responce = await HttpService.SaveResultInfo(_data);
+            string str = responce.result.ToString();
+            PlayerPrefs.SetString("ResponceResult", str);            
+
+            //Debug.Log(responce.result);
             //Debug.Log(_data.eventSq + " " + _data.roundNo + " " + _data.memberId + " " + _data.gameId + " " + _data.holeNo + " " + _data.hole + " " + _data.star);
+        }
+
+        public void WriteText()
+        {
+            if (PlayerPrefs.GetString("ResponceResult") != null)
+            {
+                ServerResponceText.text = PlayerPrefs.GetString("ResponceResult");
+            }
+            else
+            {
+                ServerResponceText.text = "null";
+            }
+
+            //if (PlayerPrefs.GetString("eventSq") != null)
+            //{
+            //    ServerText1.text = PlayerPrefs.GetString("eventSq");
+            //    ServerText2.text = PlayerPrefs.GetString("roundNo");
+            //    ServerText3.text = PlayerPrefs.GetString("memberId");
+            //    ServerText4.text = PlayerPrefs.GetString("gameId");
+            //    ServerText5.text = PlayerPrefs.GetString("holeNo");
+            //    ServerText6.text = PlayerPrefs.GetString("hole");
+            //    ServerText7.text = PlayerPrefs.GetString("star");
+            //}
         }
         
     }
